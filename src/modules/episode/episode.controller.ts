368b7268e7e2f4939fa3c5dacf9852085fa7a51b
Andi Kurniawan <andikurniawan.9a@gmail.com>
2024-06-01 06:25:20 +0700

    fix naming issue in linux

--- src/modules/episode/episode.controller.ts
+++ src/modules/episode/episode.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { EpisodeService } from './episode.service';
-import { GetUser } from 'src/core/decorators/GetUser.Decorator';
+import { GetUser } from 'src/core/decorators/GetUser.decorator';
 import { User } from '../user/user.entity';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('episode')
 @UseGuards(AuthGuard())
 export class EpisodeController {
-  constructor(private readonly episdoeService: EpisodeService) {}
+  constructor(private readonly episdoeService: EpisodeService) { }
   @Get(':id')
   async episode(@Param('id') id: string, @GetUser() user: User) {
     const data = await this.episdoeService.episode(id, user);
